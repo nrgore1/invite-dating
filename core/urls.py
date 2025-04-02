a7d2ae9cde32405c4890c56099231888b6a2373e
nrgore1 <39716979+nrgore1@users.noreply.github.com>
2025-04-02 12:05:49 -0400

    referrer is working niw

--- core/urls.py
+++ core/urls.py
@@ -4,20 +4,18 @@ from .views import register_referrer
 from .views import home, candidate_inquiry, thank_you, register_candidate, create_profile, profile_preview
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('inquiry/', candidate_inquiry, name='candidate_inquiry'),
-    path('thank-you/', views.thank_you, name='thank_you'),  # ✅ Add this
-    path('register/', register_candidate, name='register_candidate'),
-    path('create-profile/', create_profile, name='create_profile'),
-    path('profile-preview/', profile_preview, name='profile_preview'),
+    path('', views.home, name='home'),  # 👈 fixes the "home" route
+    path('register-referrer/', views.register_referrer, name='register_referrer'),
+    path('register/', views.register_candidate, name='register_candidate'),
+    path('thank-you/', views.thank_you, name='thank_you'),
+    path('create-profile/', views.create_profile, name='create_profile'),
+    path('profile-preview/', views.profile_preview, name='profile_preview'),
+     path('inquiry/', views.candidate_inquiry, name='candidate_inquiry'),
 ]
 
 urlpatterns += [
     path('create-profile/', create_profile, name='create_profile'),
 ]
 
-urlpatterns = [
-    path('register-referrer/', register_referrer, name='register_referrer'),
-    # other paths...
-]
+
 
