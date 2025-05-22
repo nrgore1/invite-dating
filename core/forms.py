b786e754d54a8c8464a21b09f253d83230caed43
nrgore1 <39716979+nrgore1@users.noreply.github.com>
2025-05-22 17:52:47 -0400

    Initial commit - Invite-Only Dating App

--- core/forms.py
+++ core/forms.py
@@ -31,9 +31,3 @@ class SearchForm(forms.Form):
     )
     location = forms.CharField(max_length=100, required=False)
 
-# core/forms.py
-
-from django import forms
-
-class SearchForm(forms.Form):
-    query = forms.CharField(max_length=100, required=False, label="Search")
