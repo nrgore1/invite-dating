e1fbcd48f90d3dadae87795c57885d0849603e03
nrgore1 <39716979+nrgore1@users.noreply.github.com>
2025-05-22 17:36:50 -0400

    Initial commit - Invite-Only Dating App

--- invite_dating/urls.py
+++ invite_dating/urls.py
@@ -3,14 +3,14 @@ from django.urls import path, include
 from core.views import register_candidate  # 👈 Add this
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import run_setup_commands
-from .views import CustomLoginView
+# from core.views import run_setup_commands
+from core.views import CustomLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # 👈 ensure this is pointing to your app
     path('register/', register_candidate, name='register'),  # 👈 Add this
-    path('run-setup/', run_setup_commands),  # ⚠️ REMOVE THIS AFTER ONE USE
+  #  path('run-setup/', run_setup_commands),  # ⚠️ REMOVE THIS AFTER ONE USE
 ]
 
 
